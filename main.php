96aaa7b8e68fee862e93a6fb83ad2f6c82dc82df
Lyle Rolleman <lrollema@ualberta.ca>
2014-08-17 15:09:09 -0600

    style, init form, init player

---
+++ main.php
@@ -0,0 +1,47 @@
+<?php
+	$xdim = $_POST["xdim"];
+	$ydim = $_POST["ydim"];
+	$playercount = 0;
+	$player1 = null;
+	$player2 = null;
+	$player3 = null;
+	$player4 = null;
+	$player1score = 0;
+	$player2score = 0;
+	$player3score = 0;
+	$player4score = 0;
+?>
+
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="labstyle.css">
+	</head>
+	<body>
+		<table>
+			<tr>
+				<?php
+					if ($_POST["player1"]) {
+						$player1 = $_POST["player1"];
+						$playercount++;
+						echo "<td><p>" . $player1 . ": $player1score </p></td>";
+					}
+					if ($_POST["player2"]) {
+						$player2 = $_POST["player2"];
+						$playercount++;
+						echo "<td><p>" . $player2 . ": $player2score </p></td>";
+					}
+					if ($_POST["player3"]) {
+						$player3 = $_POST["player3"];
+						$playercount++;
+						echo "<td><p>" . $player3 . ": $player3score </p></td>";
+					}
+					if ($_POST["player4"]) {
+						$player4 = $_POST["player4"];
+						$playercount++;
+						echo "<td><p>" . $player4 . ": $player4score </p></td>";
+					}
+				?>
+			</tr>
+		</table>
+	</body>
+</html>
---
+++ start.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="labstyle.css">
+</head>
+<body>
+	<form action="main.php" method="POST">
+	<table>
+		<tr>
+			<td>
+				<p>Player 1: </p>
+			</td>
+			<td>
+				<input type="text" name="player1">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<p>Player 2: </p>
+			</td>
+			<td>
+				<input type="text" name="player2">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<p>Player 3: </p>
+			</td>
+			<td>
+				<input type="text" name="player3">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<p>Player 4: </p>
+			</td>
+			<td>
+				<input type="text" name="player4">
+			</td>
+		</tr>
+		
+		<tr></tr>
+		<tr>
+			<td>
+				<p>Gridsize: </p>
+			</td>
+			<td>
+				<input type="number" name="xdim" value="6">
+			</td>
+			<td>
+				<input type="number" name="ydim" value="6">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<input type="submit">
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
